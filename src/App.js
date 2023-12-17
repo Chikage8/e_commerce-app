275b96c0b3b694181502467512abb9015fe43190
Chikage8 <cemgocer@gmail.com>
2023-12-18 01:14:14 +0300

    Started implementing a dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar.jsx';
+import NavItem from './components/NavItem.jsx';
+import { ReactComponent as ClothingIcon } from './icons/clothing.svg';
+import { ReactComponent as CaretIcon } from './icons/caret.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar>
+        <NavItem icon={<ClothingIcon/>}/>
+        <NavItem icon="😊"/>
+        <NavItem icon="😊"/>        
+
+        <NavItem icon={<CaretIcon/>}>
+          <p>Hello World</p>
+        </NavItem>
+      </Navbar>
     </div>
   );
 }
