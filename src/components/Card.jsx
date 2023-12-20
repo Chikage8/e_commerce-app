f96a0a6882c1406b1cfecad4e25959b9e98db861
Chikage8 <cemgocer@gmail.com>
2023-12-20 16:43:45 +0300

    Fixed the layout and sizing of the cards, added gradient background

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,13 +2,11 @@ import React from "react";
 
 function Card(props) {
     return (
-        <div className="content-section">
-            <div className="card">
+        <div className="card">
                 <h3 className="card-title">{props.title}</h3>
                 <img className="card-image" src={props.img} alt="product" />
                 <button className="card-interested-button">See more</button>
-            </div>
-        </div>
+        </div>        
     );
 }
 
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -2,7 +2,13 @@ import Card from '../components/Card.jsx';
 
 function Content() {
     return(
-        <Card title='Gaming accessories' img='https://images.unsplash.com/photo-1618339220157-daa2cd9ade56?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29tcHV0ZXIlMjBjYXNlfGVufDB8fDB8fHww'/>
+        <section className='content-section'>
+            <Card title='Gaming accessories' img='https://images.unsplash.com/photo-1618339220157-daa2cd9ade56?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Y29tcHV0ZXIlMjBjYXNlfGVufDB8fDB8fHww'/>
+            <Card title='Creating business solutions' img='https://images-na.ssl-images-amazon.com/images/G/01/us-manual-merchandising/RBS-in-house-Graphics/cc_359_laptop_us_v1._SY304_CB576754001_.jpg' />
+            <Card title='Toys under $25' img='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v3._SY304_CB573698005_.jpg' />
+            <Card title='Laptops for every need' img='https://images-na.ssl-images-amazon.com/images/G/01/us-manual-merchandising/XCM_CUTTLE_1622894_3373440_379x304_1X_en_size1_US._SY304_CB597469214_.jpg' />
+            <Card title='Deals on shoes' img='https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2023/EBF23/Fuji_Desktop_Single_image_EBF_1x_v8._SY304_CB573698005_.jpg' />
+        </section>
     );
 }
 
