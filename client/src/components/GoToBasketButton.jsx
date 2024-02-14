acd6aa659ede51ee4782c86d97172834f7d6e373
Chikage8 <cemgocer@gmail.com>
2024-02-14 13:57:28 +0300

    GoToBasketButton functionality has been implemented, styling is missing

--- client/src/components/GoToBasketButton.jsx
+++ client/src/components/GoToBasketButton.jsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import { useNavigate } from "react-router-dom";
 
 const GoToBasketButton = () => {
+
+  const navigate = useNavigate()
+
+  const handleClick = () => {
+    navigate('/basket')
+  }
+
   return (
-    <div>GoToBasketButton</div>
+    <div id='go-to-basket-button-container'>
+      <button id='go-to-basket-button' onClick={handleClick}>Go To Basket</button>
+    </div>
   )
 }
 
 export default GoToBasketButton
+
+
+// import React from 'react'
+// import { useNavigate } from "react-router-dom";
+
+// const AddToBasketButton = (props) => {
+//   const navigate = useNavigate();
+
+//   function handleClick() {
+//     localStorage.setItem(`basket/${props.id}`, {main_image: props.main_image, 
+//                                                 current_price: props.current_price})
+//     navigate("/added");
+//   }
+
+//   return (
+//     <div className='buy-button-div'>
+//         <button id='add-to-basket-button' type='submit' onClick={handleClick}>Add To Basket</button>
+//     </div>
+//   )
+// }
+
+// export default AddToBasketButton
