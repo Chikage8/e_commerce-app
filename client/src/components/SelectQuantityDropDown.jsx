dffca482a251dd639a0480562ae1dfa5a145d91c
Chikage8 <cemgocer@gmail.com>
2024-02-15 20:35:53 +0300

    All the functionality and styling for the SelectQuantityDropDown has been implemented

--- client/src/components/SelectQuantityDropDown.jsx
+++ client/src/components/SelectQuantityDropDown.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { Children } from 'react'
 
 const SelectQuantityDropDown = (props) => {
 
+    const handleClick = (e) => {
+        props.setQuantity(e.target.textContent);
+        props.setSelectQuantity(false);
+    }
+
     return (
         <div id='quantity-selector-dropdown'> 
-            <p onClick={() => {props.setQuantity(1);props.setSelectQuantity(false);}}>1</p>
-            <p onClick={() => {props.setQuantity(2);props.setSelectQuantity(false);}}>2</p>
+            <div onClick={handleClick} className='select-quantity-dropdown-option small-top-margin'><p>1</p></div>
+            <div onClick={handleClick} className='select-quantity-dropdown-option'><p>2</p></div>
+            <div onClick={handleClick} className='select-quantity-dropdown-option'><p>3</p></div>
+            <div onClick={handleClick} className='select-quantity-dropdown-option'><p>4</p></div>
+            <div onClick={handleClick} className='select-quantity-dropdown-option'><p>5</p></div>
         </div>
     )
 }
