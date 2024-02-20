8162e458dd3aa3dce2301861fdf2233bb0cef991
Chikage8 <cemgocer@gmail.com>
2024-02-20 18:29:53 +0300

    Implemented the ShoppingBasketIcon quantity counting and displaying

--- client/src/components/AddToBasketButton.jsx
+++ client/src/components/AddToBasketButton.jsx
@@ -31,27 +31,24 @@ const AddToBasketButton = (props) => {
           console.log("bb")
           let newProd = true
           for ( let i = 0; i < user.basket.length; i++ ) {
-            
-            console.log(user.basket[i][thisItemId].quantity)
-            if(Object.keys(user.basket[i])[0] == thisItemId) {
+            if(user.basket[i].id == thisItemId) {
               // Item was already in the basket, so just increase the quantity by 1
               newProd = false
-              console.log(user.basket[i][thisItemId])
-              user.basket[i][thisItemId].quantity+=1
+              user.basket[i].quantity+=1
               sessionStorage.setItem("user", JSON.stringify(user))
               break
             }
           }
           if (newProd) {
             console.log("cc")
-            let newProdObj = {[thisItemId]: {main_image: props.main_image, current_price: props.current_price, quantity: 1}}
+            let newProdObj = {id: thisItemId ,main_image: props.main_image, current_price: props.current_price, quantity: 1}
             user.basket.push(newProdObj)
             sessionStorage.setItem("user", JSON.stringify(user))
           }
         } else {
             console.log("dd")
           // user is present but the user.basket has not yet created
-          let newProdObj = {[thisItemId]: {main_image: props.main_image, current_price: props.current_price, quantity: 1}}
+          let newProdObj = {id: thisItemId ,main_image: props.main_image, current_price: props.current_price, quantity: 1}
           user.basket = [newProdObj]
           sessionStorage.setItem("user", JSON.stringify(user))
           console.log(user)
--- client/src/components/ShoppingBasketIcon.jsx
+++ client/src/components/ShoppingBasketIcon.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
 import { ReactComponent as Basket } from "../icons/shopping-basket.svg";
+import { UserContext } from "../App";
 
 const ShoppingBasketIcon = (props) => {
+
+  const [user, setUser] = useContext(UserContext)
+
+  let totalItemsInBasket = 0
+  // loop the user basket and add quantities for acquiring number of total items in the basket
+  if (user && user.basket) {
+    for (let i = 0; i < user.basket.length ; i++) {
+      totalItemsInBasket += user.basket[i].quantity
+    }
+  }
+
   return (
     <div id="shoppingbasket-component-container">
       <a href="/basket" className="clickable">
@@ -11,7 +23,7 @@ const ShoppingBasketIcon = (props) => {
           </div>
           <p> My Basket </p>
           <div id="basketItemCount-container">
-            <div> {props.quantity} </div>
+            <div> {totalItemsInBasket} </div>
           </div>
         </div>
       </a>
