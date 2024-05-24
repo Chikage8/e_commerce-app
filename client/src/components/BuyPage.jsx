66df62245f47712b3f809481e40d2ac463ff906a
Chikage8 <cemgocer@gmail.com>
2024-05-24 19:12:21 +0300

    Added link to amazon.com on the buypage

--- client/src/components/BuyPage.jsx
+++ client/src/components/BuyPage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const BuyPage = () => {
   return (
-    <div>BuyPage</div>
+    <div id='buy-page'>
+      <h1 id='buy-page-header'>This is just a sample website, please go to <a id='amazon-link' href='https://www.amazon.com/'>amazon.com</a> if you have interest in the products.</h1>
+    </div>
   )
 }
 
